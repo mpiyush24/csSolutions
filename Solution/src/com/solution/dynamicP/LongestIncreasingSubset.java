547da123aa1d4e57591850767204c6f2384a244e
Piyush <piyushmaheshwari19@gmail.com>
2017-07-02 18:30:37 +0530

    LongetIncreasing Subset, LongestPalindromeSubsequence

---
+++ Solution/src/com/solution/dynamicP/LongestIncreasingSubset.java
@@ -0,0 +1,46 @@
+package com.solution.dynamicP;
+
+import java.util.Scanner;
+
+import com.solution.main.ReadInput;
+
+public class LongestIncreasingSubset {
+	public void run() {
+
+		Scanner sc = ReadInput.getScanner("longestIncreasingSubset");
+
+		int size = sc.nextInt();
+
+		int[] arr = ReadInput.getIntArray(size, sc.next());
+
+		System.out.println(doRun(arr));
+	}
+
+	private int doRun(int[] arr) {
+
+		int[] T = new int[arr.length];
+
+		for (int i = 0; i < T.length; i++) {
+			T[i] = 1;
+		}
+
+		for (int i = 1; i < arr.length;i++) {
+			for (int j = 0; j < i; j++) {
+				if (arr[j] < arr[i]) {
+					T[i] = Math.max(T[i], T[j] + 1);
+				}
+			}
+		}
+
+		int max = T[0];
+		for(int i = 1 ; i < T.length; i ++){
+			max = Math.max(max,T[i]);
+		}
+		
+		return max;
+	}
+	
+	
+	
+
+}
