be98d7507d8f9dff2192345125884561c58c32dd
mpiyush24 <piyushmaheshwari19@gmail.com>
2020-08-05 12:34:01 +0530

    Fix: test run via command

--- src/test/kotlin/com/cssolutions/array/LocalMaximaTest.kt
+++ src/test/kotlin/com/cssolutions/array/LocalMaximaTest.kt
@@ -1,16 +1,15 @@
 package com.cssolutions.array
 
 import com.cssolutions.main.IntResult
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 
 class LocalMaximaTest {
-
     @Test
     fun baseCase()
     {
         var intResult = IntResult()
-        val result = LocalMaxima().doMaxima(arrayOf(1, 2, 9, 7, 6, 5), 0, 5, intResult)
+        LocalMaxima().doMaxima(arrayOf(1, 2, 9, 7, 6, 5), 0, 5, intResult)
         assertEquals(9, intResult.result)
     }
 }
